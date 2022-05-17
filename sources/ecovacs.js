6b5a5c8520414f391049e796b59b01cfd13b0d83
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2022-05-17 16:29:53 -0400

    add dynamic interval selection to ecovacs source

--- sources/ecovacs.js
+++ sources/ecovacs.js
@@ -5,10 +5,30 @@ const { EcoVacsAPI, countries } = require("ecovacs-deebot");
 const nodeMachineId = require("node-machine-id");
 
 var config = null;
+
+// Data collection is initially every 45s, but
+// if the vacuum is fully charged and idle, this
+// interval is decreased to once every 5 minutes.
+// If the interval is 5 minutes and the vacuum is
+// no longer fully charged and idle, the interval
+// is reset back to 45s.
 var collectionInterval = null;
-const rejectionTimeout = 30000; // reject a data collection promise after a time
+const activeInterval = 45000;
+const passiveInterval = 300000;
+var intervalLength = activeInterval;
+
+// Reject a data collection promise after a time
+const rejectionTimeout = 30000;
 var vacuums = [];
 var sinks = {};
+
+// Small helper function to switch
+// the collection interval around.
+function switchInterval(intervalType) {
+    clearInterval(collectionInterval);
+    intervalLength = intervalType === "active" ? activeInterval : passiveInterval;
+    setInterval(collect, intervalLength);
+}
 
 const chargeModeMap = {
     idle: 0,
@@ -24,6 +44,14 @@ const cleanModeMap = {
     spot: 3,
     singleRoom: 4,
     other: 5
+}
+
+// Deebot event name : Command to trigger the event
+const events = {
+    BatteryInfo :"GetBatteryState",
+    CleanReport :"GetCleanState",
+    ChargeState :"GetChargeState",
+    LifeSpan :"GetLifeSpan"
 }
 
 // From each vacuum, get the following data:
@@ -45,14 +73,14 @@ async function collect() {
                 return null;
             }
 
-            let resolves = [null, null, null, null];
-            const promises = [0, 0, 0, 0].map(
+            let resolves = [...new Array(Object.keys(events).length)].map(_ => null);
+            const promises = resolves.map(
                 (_, index) => new Promise((resolve, reject) => {
                     resolves[index] = resolve;
-                    setTimeout(reject, rejectionTimeout);
+                    setTimeout(() => reject("request timeout"), rejectionTimeout);
                 })
                 .catch(
-                    err => console.info("data request failed for vacuum %s", vacuum.nickname)
+                    err => console.info("data request failed for vacuum %s: %s", vacuum.nickname, err)
                 )
             );
 
@@ -60,11 +88,9 @@ async function collect() {
             vacuum.event_promises = resolves;
             
             // Issue commands to the vacuum
-            const vacbot = vacuum.vacbot;
-            vacbot.run("GetBatteryState");
-            vacbot.run('GetCleanState');
-            vacbot.run('GetChargeState');
-            vacbot.run('GetLifeSpan');
+            for(const command of Object.values(events)) {
+                vacuum.vacbot.run(command);
+            }
 
             // Wait for all commands to finish or timeout
             const data = await Promise.all(promises);
@@ -104,6 +130,25 @@ async function collect() {
                 device_id: vacuum.device_id,
                 device_type: vacuum.device_type
             };
+
+            // Check to see if the interval needs
+            // to be updated (5m->45s or 45s->5m)
+            const vacuumActive = 
+                (metrics.battery !== undefined && metrics.battery >= 100) &&
+                (metrics.charge_state !== undefined && metrics.charge_state !== chargeModeMap.idle);
+            if(intervalLength === activeInterval && !vacuumActive) {
+                // If the vacuum is fully charged and
+                // cleaning status is idle, we need to
+                // switch to the passive interval.
+                switchInterval("passive");
+                console.info("switched ecovacs device '%s' to passive mode", vacuum.nickname);
+            } else if(intervalLength === passiveInterval && vacuumActive) {
+                // If the vacuum is not fully charged
+                // or the cleaning state is not idle,
+                // we need to switch to the active interval.
+                switchInterval("active");
+                console.info("switched ecovacs device '%s' to active mode", vacuum.nickname)
+            }
 
             return [metrics, metadata];
         })
@@ -162,12 +207,7 @@ module.exports = {
                         vacuum.connected = true;
                         connectResolves[vacuum._id]();
                         // Register event handlers
-                        [
-                            "BatteryInfo",
-                            "CleanReport",
-                            "ChargeState",
-                            "LifeSpan"
-                        ].map((operation, index) => {
+                        Object.keys(events).map((operation, index) => {
                             vacuum.vacbot.on(operation, value => {
                                 if(typeof vacuum.event_promises[index] === "function") {
                                     vacuum.event_promises[index](value);
@@ -186,7 +226,13 @@ module.exports = {
 
                 await Promise.all(connections);
                 collect();
-                collectionInterval = setInterval(collect, 180000); // collect data every 5 minutes
+
+                // The collect() function manages switching
+                // between the active and passive intervals.
+                // Here we can assume the active interval and
+                // the first collect() call will either keep
+                // it or switch to the passive interval.
+                switchInterval("active");
             });
         }).catch((e) => {
             console.error(e.message);
